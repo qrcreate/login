c1ea64213684e90ca42319e90025d5289303d2e8
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2025-01-16 16:44:29 +0700

    ganti client id

--- gsi.js
+++ gsi.js
@@ -7,23 +7,23 @@ import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js
    
 await addCSSInHead("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css");
    
-const url="https://asia-southeast2-awangga.cloudfunctions.net/bukupedia/auth/users";
+const url="https://asia-southeast2-qrcreate-447114.cloudfunctions.net/qrcreate/auth/users";
    
-const client_id="239713755402-4hr2cva377m43rsqs2dk0c7f7cktfeph.apps.googleusercontent.com";
+const client_id="981593361501-t6aa598ahgqq8149kst14me2cu2ihkeo.apps.googleusercontent.com";
    
 // Panggil fungsi untuk menambahkan elemen
 appendGoogleSignin(client_id,url);
    
    
 // Buat fungsi untuk memanggil gsi js dan menambahkan elemen div ke dalam DOM
-async function appendGoogleSignin(client_id, target_url) {
+async function appendGoogleSignin(client_id, url) {
     try {
         // Memuat script Google Sign-In
         await addJSInHead("https://accounts.google.com/gsi/client");
         // Menginisialisasi Google Sign-In dan menetapkan gSignIn sebagai callback
         google.accounts.id.initialize({
             client_id: client_id,
-            callback:  (response) => gSignIn(response, target_url), // Menggunakan gSignIn sebagai callback untuk Google Sign-In
+            callback:  (response) => gSignIn(response, url), // Menggunakan gSignIn sebagai callback untuk Google Sign-In
         });
         // Memunculkan pop-up Google Sign-In
         google.accounts.id.prompt();
@@ -33,10 +33,10 @@ async function appendGoogleSignin(client_id, target_url) {
     }
 }
    
-async function gSignIn(response, target_url) {
+async function gSignIn(response, url) {
     try {
         const gtoken = { token: response.credential };
-        await postJSON(target_url, "login", getCookie("login"), gtoken, responsePostFunction);
+        await postJSON(url, "login", getCookie("login"), gtoken, responsePostFunction);
     } catch (error) {
         console.error("Network or JSON parsing error:", error);
         Swal.fire({
@@ -51,7 +51,7 @@ function responsePostFunction(response) {
     if (response.status === 200 && response.data) {
         console.log(response.data);
         setCookieWithExpireHour('login',response.data.token,18);
-        redirect("/dashboard");
+        redirect("/qr");
     } else {
         console.error("Login failed:", response.data?.message || "Unknown error");
         Swal.fire({
